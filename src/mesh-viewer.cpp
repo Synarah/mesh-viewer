ed7a41b2f8b42924f9f37f588c00788290e228e0
Synarah Sitaf <synarahsitaf@wc122-dhcp-53.brynmawr.edu>
2023-03-17 00:00:44 -0400

    A04

--- src/mesh-viewer.cpp
+++ src/mesh-viewer.cpp
@@ -17,50 +17,124 @@ using namespace agl;
 
 class MeshViewer : public Window {
 public:
-   MeshViewer() : Window() {
+    MeshViewer() : Window() {
+    }
+
+    void setup() {
+        file = GetFilenamesInDir("../models", "ply");
+        for (int i = 0; i < file.size(); i++){
+            PLYMesh curFile;
+        printf("---\n beginning %d/%lu \n -----\n\n", i, file.size());
+            curFile.load("../models/" + file[i]);
+            m.push_back(curFile);
+        }
+        mesh = m[0];
+        printf("setup");
+    }
+
+    void mouseMotion(int x, int y, int dx, int dy) {
+        if (click) {
+            h = h + dy * 0.1;
+            a = a - dx * 0.1;
+            float x = rad * sin(a) * cos(h);
+            float y = rad * sin(h);
+            float z = rad * cos(a) * cos(h);
+            eyePos = vec3(x, y, z);
+        }
+    }
+
+    void mouseDown(int button, int mods) {
+        click = true;
+    }
+
+    void mouseUp(int button, int mods) {
+        click = false;
+    }
+
+    void scroll(float dx, float dy) {
+        rad = rad + dy;
+        float x = rad* sin(a) * cos(h);
+        float y = rad*sin(h);
+        float z = rad* cos(a) * cos(h);
+        eyePos = vec3(x, y, z);
+    }
+
+    void keyUp(int key, int mods) {
+      if(key == GLFW_KEY_N){
+         if(curIn == m.size() -1){
+            curIn = 0;
+         }
+         else{
+            curIn = curIn + 1;
+         }
+      }
+      else if(key == GLFW_KEY_P){
+         if(curIn == 0){
+            curIn = m.size() -1;
+         }
+         else{
+            curIn = curIn - 1;
+         }
+      }
    }
 
-   void setup() {
-      // mesh.load("../models/cube.ply");
-   }
+    void draw() {
+        std::cout << file[curIn] << std::endl;
+        mesh = m[curIn];
+        float aspect = ((float)width()) / height();
+        renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
+        renderer.lookAt(eyePos, lookPos, up);
 
-   void mouseMotion(int x, int y, int dx, int dy) {
-   }
+       
+        float sx = 1;
+        float sy = 1;
+        float sz = 1;
+        GLfloat xr = mesh.maxBounds().x - mesh.minBounds().x;
+        GLfloat yr = mesh.maxBounds().y - mesh.minBounds().y;
+        GLfloat zr = mesh.maxBounds().z - mesh.minBounds().z;
+        if ((xr > yr) || (xr > zr)) {
+            sx = 5.0f / xr;
+            sy = 5.0f / xr;
+            sz = 5.0f / xr;
+        }
+        else if (yr > zr) {
+            sx = 5.0f / yr;
+            sy = 5.0f / yr;
+            sz = 5.0f / yr;
+        }    
+        else if (zr > yr) {
+            sx = 5.0f / zr;
+            sy = 5.0f / zr;
+            sz = 5.0f / zr;
 
-   void mouseDown(int button, int mods) {
-   }
+        }
+        renderer.scale(vec3(sx, sy, sz));
 
-   void mouseUp(int button, int mods) {
-   }
+        //renderer.scale(vec3(sin(elapsedTime())*10, cos(elapsedTime())*10, -cos(elapsedTime()*10)));
+        renderer.translate(vec3(0, 0, 0));
+        renderer.lookAt(eyePos,lookPos, up);
 
-   void scroll(float dx, float dy) {
-   }
-
-   void keyUp(int key, int mods) {
-   }
-
-   void draw() {
-      float aspect = ((float)width()) / height();
-      renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
-
-      renderer.rotate(vec3(0,0,0));
-      renderer.scale(vec3(1,1,1));
-      renderer.translate(vec3(0,0,0));
-      //renderer.mesh(mesh);
-      renderer.cube(); // for debugging!
-   }
+        renderer.mesh(mesh);
+        //renderer.cube(); // for debugging!
+    }
 
 protected:
-   PLYMesh mesh;
-   vec3 eyePos = vec3(10, 0, 0);
-   vec3 lookPos = vec3(0, 0, 0);
-   vec3 up = vec3(0, 1, 0);
+    std::vector<string> file = GetFilenamesInDir("../models", "ply");
+    PLYMesh mesh;
+    std::vector<PLYMesh> m;
+    vec3 eyePos = vec3(10, 0, 0);
+    vec3 lookPos = vec3(0, 0, 0);
+    vec3 up = vec3(0, 1, 0);
+    bool click = false;
+    float h = 0;
+    float a = 0;
+    float rad = 10;
+    int curIn = 0;
 };
 
 int main(int argc, char** argv)
 {
-   MeshViewer viewer;
-   viewer.run();
-   return 0;
+    MeshViewer viewer;
+    viewer.run();
+    return 0;
 }
-
--- src/plymesh.cpp
+++ src/plymesh.cpp
@@ -5,6 +5,10 @@
 //--------------------------------------------------
 
 #include "plymesh.h"
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <sstream>
 
 using namespace std;
 using namespace glm;
@@ -32,23 +36,125 @@ namespace agl {
          return false;
       }
       // todo: your code here
+      ifstream myFile;
+      myFile.open(filename);
+
+      if(!myFile.is_open()) {
+         return false;
+      }
+
+      string info;
+      myFile >> info;
+
+      if(info.compare("ply") != 0) {
+         return false;
+      }
+
+      while(info.compare("vertex") != 0) {
+         myFile >> info;
+      }
+
+      myFile >> info;
+      int numVer = stoi(info);
+
+      while(info.compare("face") != 0) {
+         myFile >> info;
+      }
+      myFile >> info;
+      int numFace = stoi(info);
+
+      while(info.compare("end_header") != 0) {
+         myFile >> info;
+      }
+
+      for(int i = 0; i < numVer; i++) {
+         GLfloat v1, v2, v3, v4, v5, v6;
+         myFile >> v1;
+         myFile >> v2;
+         myFile >> v3;
+         myFile >> v4;
+         myFile >> v5;
+         myFile >> v6;
+         vec3 vtx = vec3(v1, v2, v3);
+
+         _positions.push_back(v1);
+         _positions.push_back(v2);
+         _positions.push_back(v3);
+         _normals.push_back(v4);
+         _normals.push_back(v5);
+         _normals.push_back(v6);
+
+         myFile >> info;
+         myFile >> info;
+      }
+
+      for(int i = 0; i < numFace; i++) {
+         myFile >> info;
+         GLuint  f1, f2, f3;
+         myFile >> f1;
+         myFile >> f2;
+         myFile >> f3;
+         _faces.push_back(f1);
+         _faces.push_back(f2);
+         _faces.push_back(f3);
+         for(int j = 0; j < numFace - 3; j++){
+            GLuint fi;
+            myFile >> fi;
+            _faces.push_back(f1 - 1);
+            _faces.push_back(fi - 1);
+            _faces.push_back(fi - 1); 
+         }
+      }
+
+      if(numVer == 0 || numFace == 0){
+         return false;
+      }
+
       return false;
    }
 
    glm::vec3 PLYMesh::minBounds() const {
-      return glm::vec3(0);
+      glm::vec3 min(_positions[0], _positions[1], _positions[2]);
+
+      for(int i = 0; i < _positions.size(); i += 3){
+      glm::vec3 ver(_positions[i], _positions[i+1], _positions[i+2]);
+          if(ver.x < min.x){
+            min.x = ver.x;
+         }
+         if(ver.y < min.y){
+            min.y = ver.y;
+         }
+         if(ver.z < min.z){
+            min.z = ver.z;
+         }
+      }
+      return min;
    }
 
    glm::vec3 PLYMesh::maxBounds() const {
-      return glm::vec3(0);
+      glm::vec3 max(_positions[0], _positions[1], _positions[2]);
+
+       for (int i = 0; i < _positions.size(); i += 3){
+         glm::vec3 ver(_positions[i], _positions[i+1], _positions[i+2]);
+         if(ver.x > max.x){
+            max.x = ver.x;
+         }
+         if(ver.y > max.y){
+            max.y = ver.y;
+         }
+         if(ver.z > max.z){
+            max.z = ver.z;
+         }
+       }
+      return max;
    }
 
    int PLYMesh::numVertices() const {
-      return _positions.size();
+      return _positions.size()/3;
    }
 
    int PLYMesh::numTriangles() const {
-      return _faces.size();
+      return _faces.size()/3;
    }
 
    const std::vector<GLfloat>& PLYMesh::positions() const {
@@ -61,5 +167,11 @@ namespace agl {
 
    const std::vector<GLuint>& PLYMesh::indices() const {
       return _faces;
+   }
+
+   void PLYMesh::clear(){
+      _positions.clear();
+      _normals.clear();
+      _faces.clear();
    }
 }
